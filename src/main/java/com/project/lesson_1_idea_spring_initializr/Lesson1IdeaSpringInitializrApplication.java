29f1fb8515f453a94d439b6353a6a52da1c85bcf
MoskvichevViktor <beldetal@yandex.ru>
2021-11-04 15:10:43 +0300

    start

---
+++ src/main/java/com/project/lesson_1_idea_spring_initializr/Lesson1IdeaSpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.project.lesson_1_idea_spring_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson1IdeaSpringInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson1IdeaSpringInitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/lesson_1_idea_spring_initializr/Lesson1IdeaSpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lesson_1_idea_spring_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson1IdeaSpringInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
